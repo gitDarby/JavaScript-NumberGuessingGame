7d0ad430bba5a13ca705f2b3918780f35fdeb8aa
gitDarby <webdev.dm@outlook.com>
2026-01-18 13:22:53 -0500

    Game complete

--- script.js
+++ script.js
@@ -8,3 +8,43 @@
     - If tie, player wins
  4. Play game
  */
+
+ const playerChoice = (playerInput) => {
+    if (playerInput) {
+        return playerInput;
+    } else {
+        return null;
+    }
+ }
+
+
+
+ const computerChoice = () => {
+    let computerInput = Math.floor(Math.random() * 11)
+    return computerInput;
+ }
+
+
+
+ const compareChoices = (playerInput, computerInput) => {
+    if (playerInput >= computerInput) {
+        return 'Player wins!';
+    } else {
+        return 'Computer wins!';
+    }
+ }
+
+ const playGame = () => {
+    let playerInput = playerChoice(7);
+    if (playerInput === null) {
+        return console.log('Enter a number');
+    }
+
+    let computerInput = computerChoice();
+
+    let results = compareChoices(playerInput, computerInput);
+
+    return console.log(`Players number: ${playerInput}, Computers number: ${computerInput}. The results are in: ${results}`);
+ }
+
+ playGame();
