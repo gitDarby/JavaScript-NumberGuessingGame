28459ceb8e3f77292c936b1229a2099190456439
gitDarby <webdev.dm@outlook.com>
2026-01-18 11:45:43 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ NUMBER GUESING GAME
+ 1. Player chooses a number;
+ 2. Computer chooses a number;
+    - random number generates.
+ 3. Compare choices;
+    - Greater number wins.
+    - If tie, player wins
+ 4. Play game
+ */
